25bdd51ed882fece4376494d255d701f13ec781e
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-10-15 09:48:21 -0400

    added the method to return my name

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,15 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        System.out.println("Hello World");
+        Printperson(" mohamed");
+    }
+    public static void Printperson(String name){
+        System.out.println("Hello"+ name);
+
     }
 }
