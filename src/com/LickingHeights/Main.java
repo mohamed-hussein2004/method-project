3fbb1070a5699fbf40261185a09a59724a2c8120
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-10-17 10:05:56 -0400

    Added the 2 # that need to be multiplied

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,15 +1,29 @@
 package com.LickingHeights;
 
-import java.util.Scanner;
-
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         System.out.println("Hello World");
         Printperson(" mohamed");
+        int a,d;
+        a = 5;
+        d = 7;
+        System.out.println(a*d`);
+
     }
     public static void Printperson(String name){
         System.out.println("Hello"+ name);
 
+    }
+    public static int multiply(int c){
+    int a,d;
+    a = 5;
+    d = 7;
+    c = a*d;
+    System.out.println(c);
+
+
+
+        return c;
     }
 }
