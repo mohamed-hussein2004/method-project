b9cbcd6599eef47d80bf0b3a1a7de2b3d6ddbe6a
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-10-17 10:24:02 -0400

    added the 2 # that need to be divided

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,22 +8,23 @@ public class Main {
         int a,d;
         a = 5;
         d = 7;
-        System.out.println(a*d`);
-
+        System.out.println(a*d);
+        double c,b;
+        c = 70;
+        b = 30;
+        System.out.println(c/b);
     }
     public static void Printperson(String name){
         System.out.println("Hello"+ name);
 
     }
-    public static int multiply(int c){
-    int a,d;
-    a = 5;
-    d = 7;
-    c = a*d;
-    System.out.println(c);
+    public static int multiply(int a, int d){
+        return a*d;
+    }
+        public static double divde(double c, double b){
+        c = 70;
+        b = 30;
+     return c/b;
 
-
-
-        return c;
     }
 }
